519a49459d21832afcbc512bdb13bcf7820f591c
Raghav Aggarwal <raghavaggarwal776@gmail.com>
2023-03-06 19:25:51 +0530

    Initial commit

---
+++ app/src/main/java/com/example/threadvscoroutine/CoroutineActivity.kt
@@ -0,0 +1,47 @@
+package com.example.threadvscoroutine
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.threadvscoroutine.databinding.ActivityCoroutineBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+// Activity which contains a button to spawn 100_000 Coroutines
+class CoroutineActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCoroutineBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityCoroutineBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            launchCoroutines()
+        }
+    }
+
+    private fun launchCoroutines() {
+        // create a separate coroutine to launch other coroutines
+        // as not to block Main Thread
+        CoroutineScope(Dispatchers.Default).launch {
+            repeat(100_000) {
+                launch {
+                    delay(5000)
+                    withContext(Dispatchers.Main) {
+                        "coroutine number- $it".also { binding.textview.text = it }
+                    }
+                    Log.d(TAG, Thread.currentThread().name)
+                }
+            }
+            Log.d(TAG, "finished")
+        }
+    }
+
+    companion object {
+        private const val TAG = "CoroutineActivity"
+    }
+}
---
+++ app/src/main/java/com/example/threadvscoroutine/ThreadActivity.kt
@@ -0,0 +1,58 @@
+package com.example.threadvscoroutine
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.threadvscoroutine.databinding.ActivityThreadBinding
+
+// Activity which contains a button to spawn 100_000 Threads
+class ThreadActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityThreadBinding
+    private val handler: Handler = object : Handler(Looper.getMainLooper()) {
+        override fun handleMessage(message: Message) {
+            super.handleMessage(message)
+            "thread number - ${message.obj}".also { binding.textview.text = it }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityThreadBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            val thread = ExampleThread()
+            thread.start()
+        }
+    }
+
+    // I am using another thread to start 100,000 threads
+    // as so the UI can be updated.
+    inner class ExampleThread : Thread() {
+        override fun run() {
+            startThreads()
+        }
+    }
+
+    // creates and starts 100,000 threads
+    private fun startThreads() {
+        repeat(100_000) {
+            val thread = Thread {
+                Thread.sleep(5000)
+                Log.d(TAG, Thread.currentThread().name)
+                val message = Message.obtain()
+                message.obj = it
+                handler.sendMessage(message)
+            }
+            thread.start()
+        }
+    }
+
+    companion object {
+        private const val TAG = "ThreadActivity"
+    }
+}
